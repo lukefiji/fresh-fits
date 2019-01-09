8acfcd07875faf0f7c76dad38f9a843a22be0c7f
Luke Fiji <luke.fiji@gmail.com>
2019-01-08 23:25:33 -0800

    Complete video 14 - Item creation and prisma yoga flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,21 @@
-const Mutations = {};
+const Mutations = {
+  // createItem returns a promise
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    // This mutation is available in prisma.graphql
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      // Pass query parameters to backend
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  // If there is no auth requried, you can forward from Prisma
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   // This query is available in prisma.graphql
+  //   // It's fine to return a promise in a resolver
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
