39256721afc5b3b53c26bcc5145dac9ec16efbc5
Luke Fiji <luke.fiji@gmail.com>
2019-01-08 23:43:38 -0800

    Complete video 15 - setting up apollo client with react

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,36 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 // This component is wrapped around every page to keep persistent state
 class MyApp extends App {
+  // To get SSR working
+  static async getInitialProps({ Component, ctx }) {
+    // Crawls all pages, fetches data, and returns here
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // This exposes the query to the user
+    pageProps.query = ctx.query;
+    // Returning to expose via props
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
